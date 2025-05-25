2ae304292a58a31cd14959e1f9892b0b65eac902
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2025-05-25 21:49:47 +0000

    Update docs/spin/main

--- docs/spin/main/trait.impl/spin_world/wasi/keyvalue/atomics/trait.HostCas.js
+++ docs/spin/main/trait.impl/spin_world/wasi/keyvalue/atomics/trait.HostCas.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["spin_factor_key_value",[["impl <a class=\"trait\" href=\"spin_world/wasi/keyvalue/atomics/trait.HostCas.html\" title=\"trait spin_world::wasi::keyvalue::atomics::HostCas\">HostCas</a> for <a class=\"struct\" href=\"spin_factor_key_value/struct.KeyValueDispatch.html\" title=\"struct spin_factor_key_value::KeyValueDispatch\">KeyValueDispatch</a>"]]],["spin_world",[]]]);
+    var implementors = Object.fromEntries([["spin_factor_key_value",[["impl HostCas for <a class=\"struct\" href=\"spin_factor_key_value/struct.KeyValueDispatch.html\" title=\"struct spin_factor_key_value::KeyValueDispatch\">KeyValueDispatch</a>"]]],["spin_world",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[351,18]}
+//{"start":57,"fragment_lengths":[206,18]}
