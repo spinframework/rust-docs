af129dc02cd667b5fae5b6b51334b184eed23621
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2025-06-06 00:53:33 +0000

    Update docs/spin/main

--- docs/spin/main/trait.impl/spin_factors/runtime_factors/trait.HasInstanceBuilder.js
+++ docs/spin/main/trait.impl/spin_factors/runtime_factors/trait.HasInstanceBuilder.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["spin_runtime_factors",[["impl HasInstanceBuilder for <a class=\"struct\" href=\"spin_runtime_factors/struct.TriggerFactorsInstanceBuilders.html\" title=\"struct spin_runtime_factors::TriggerFactorsInstanceBuilders\">TriggerFactorsInstanceBuilders</a>"]]]]);
+    var implementors = Object.fromEntries([["spin_runtime_factors",[["impl <a class=\"trait\" href=\"spin_factors/runtime_factors/trait.HasInstanceBuilder.html\" title=\"trait spin_factors::runtime_factors::HasInstanceBuilder\">HasInstanceBuilder</a> for <a class=\"struct\" href=\"spin_runtime_factors/struct.TriggerFactorsInstanceBuilders.html\" title=\"struct spin_runtime_factors::TriggerFactorsInstanceBuilders\">TriggerFactorsInstanceBuilders</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[256]}
+//{"start":57,"fragment_lengths":[413]}
--- docs/spin/main/trait.impl/spin_factors_executor/trait.ComponentLoader.js
+++ docs/spin/main/trait.impl/spin_factors_executor/trait.ComponentLoader.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["spin_trigger",[["impl&lt;T: RuntimeFactors, U&gt; ComponentLoader&lt;T, U&gt; for <a class=\"struct\" href=\"spin_trigger/loader/struct.ComponentLoader.html\" title=\"struct spin_trigger::loader::ComponentLoader\">ComponentLoader</a>"]]]]);
+    var implementors = Object.fromEntries([["spin_trigger",[["impl&lt;T: <a class=\"trait\" href=\"spin_factors/runtime_factors/trait.RuntimeFactors.html\" title=\"trait spin_factors::runtime_factors::RuntimeFactors\">RuntimeFactors</a>, U&gt; <a class=\"trait\" href=\"spin_factors_executor/trait.ComponentLoader.html\" title=\"trait spin_factors_executor::ComponentLoader\">ComponentLoader</a>&lt;T, U&gt; for <a class=\"struct\" href=\"spin_trigger/loader/struct.ComponentLoader.html\" title=\"struct spin_trigger::loader::ComponentLoader\">ComponentLoader</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[239]}
+//{"start":57,"fragment_lengths":[524]}
