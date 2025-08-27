3eb7bfb8b866f39d79d9da2d92b4352b488e7fa7
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2025-08-27 21:29:21 +0000

    Update docs/spin/main

--- docs/spin/main/trait.impl/spin_trigger/trait.Trigger.js
+++ docs/spin/main/trait.impl/spin_trigger/trait.Trigger.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["spin_trigger",[]],["spin_trigger_http",[["impl&lt;F: RuntimeFactors&gt; <a class=\"trait\" href=\"spin_trigger/trait.Trigger.html\" title=\"trait spin_trigger::Trigger\">Trigger</a>&lt;F&gt; for <a class=\"struct\" href=\"spin_trigger_http/struct.HttpTrigger.html\" title=\"struct spin_trigger_http::HttpTrigger\">HttpTrigger</a>"]]],["spin_trigger_redis",[["impl&lt;F: RuntimeFactors&gt; Trigger&lt;F&gt; for <a class=\"struct\" href=\"spin_trigger_redis/struct.RedisTrigger.html\" title=\"struct spin_trigger_redis::RedisTrigger\">RedisTrigger</a>"]]]]);
+    var implementors = Object.fromEntries([["spin_trigger",[]],["spin_trigger_http",[["impl&lt;F: RuntimeFactors&gt; Trigger&lt;F&gt; for <a class=\"struct\" href=\"spin_trigger_http/struct.HttpTrigger.html\" title=\"struct spin_trigger_http::HttpTrigger\">HttpTrigger</a>"]]],["spin_trigger_redis",[["impl&lt;F: RuntimeFactors&gt; Trigger&lt;F&gt; for <a class=\"struct\" href=\"spin_trigger_redis/struct.RedisTrigger.html\" title=\"struct spin_trigger_redis::RedisTrigger\">RedisTrigger</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[19,316,220]}
+//{"start":57,"fragment_lengths":[19,214,220]}
