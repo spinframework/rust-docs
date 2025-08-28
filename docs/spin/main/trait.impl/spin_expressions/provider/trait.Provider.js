5e10c0706e1ccbb7639fb05b39634dfcc84d812b
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2025-08-28 20:56:40 +0000

    Update docs/spin/main

--- docs/spin/main/trait.impl/spin_expressions/provider/trait.Provider.js
+++ docs/spin/main/trait.impl/spin_expressions/provider/trait.Provider.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["spin_variables_azure",[["impl Provider for <a class=\"struct\" href=\"spin_variables_azure/struct.AzureKeyVaultProvider.html\" title=\"struct spin_variables_azure::AzureKeyVaultProvider\">AzureKeyVaultProvider</a>"]]],["spin_variables_env",[["impl Provider for <a class=\"struct\" href=\"spin_variables_env/struct.EnvVariablesProvider.html\" title=\"struct spin_variables_env::EnvVariablesProvider\">EnvVariablesProvider</a>"]]],["spin_variables_static",[["impl <a class=\"trait\" href=\"spin_expressions/provider/trait.Provider.html\" title=\"trait spin_expressions::provider::Provider\">Provider</a> for <a class=\"struct\" href=\"spin_variables_static/struct.StaticVariablesProvider.html\" title=\"struct spin_variables_static::StaticVariablesProvider\">StaticVariablesProvider</a>"]]],["spin_variables_vault",[["impl Provider for <a class=\"struct\" href=\"spin_variables_vault/struct.VaultVariablesProvider.html\" title=\"struct spin_variables_vault::VaultVariablesProvider\">VaultVariablesProvider</a>"]]]]);
+    var implementors = Object.fromEntries([["spin_variables_azure",[["impl Provider for <a class=\"struct\" href=\"spin_variables_azure/struct.AzureKeyVaultProvider.html\" title=\"struct spin_variables_azure::AzureKeyVaultProvider\">AzureKeyVaultProvider</a>"]]],["spin_variables_env",[["impl Provider for <a class=\"struct\" href=\"spin_variables_env/struct.EnvVariablesProvider.html\" title=\"struct spin_variables_env::EnvVariablesProvider\">EnvVariablesProvider</a>"]]],["spin_variables_static",[["impl Provider for <a class=\"struct\" href=\"spin_variables_static/struct.StaticVariablesProvider.html\" title=\"struct spin_variables_static::StaticVariablesProvider\">StaticVariablesProvider</a>"]]],["spin_variables_vault",[["impl Provider for <a class=\"struct\" href=\"spin_variables_vault/struct.VaultVariablesProvider.html\" title=\"struct spin_variables_vault::VaultVariablesProvider\">VaultVariablesProvider</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[219,211,360,223]}
+//{"start":57,"fragment_lengths":[219,211,229,223]}
