f2793b5fdc27e3de853b671d5dc72b02a123e95d
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2025-08-28 18:43:47 +0000

    Update docs/spin/v3.4

--- docs/spin/v3.4/trait.impl/wasmtime/runtime/store/context/trait.AsContext.js
+++ docs/spin/v3.4/trait.impl/wasmtime/runtime/store/context/trait.AsContext.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["spin_core",[["impl&lt;T: 'static&gt; <a class=\"trait\" href=\"https://docs.rs/wasmtime/36.0.1/wasmtime/runtime/store/context/trait.AsContext.html\" title=\"trait wasmtime::runtime::store::context::AsContext\">AsContext</a> for <a class=\"struct\" href=\"spin_core/struct.Store.html\" title=\"struct spin_core::Store\">Store</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["spin_core",[["impl&lt;T: 'static&gt; <a class=\"trait\" href=\"https://docs.rs/wasmtime/36.0.2/wasmtime/runtime/store/context/trait.AsContext.html\" title=\"trait wasmtime::runtime::store::context::AsContext\">AsContext</a> for <a class=\"struct\" href=\"spin_core/struct.Store.html\" title=\"struct spin_core::Store\">Store</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
