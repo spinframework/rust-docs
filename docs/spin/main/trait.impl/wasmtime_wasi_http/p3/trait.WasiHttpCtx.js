9ab379562b039ce264d19dcac74903f5c2c67bb8
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2026-03-23 23:06:56 +0000

    Update docs/spin/main

--- docs/spin/main/trait.impl/wasmtime_wasi_http/p3/trait.WasiHttpCtx.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["spin_factor_outbound_http",[["impl <a class=\"trait\" href=\"https://docs.rs/wasmtime-wasi-http/42.0.1/wasmtime_wasi_http/p3/trait.WasiHttpCtx.html\" title=\"trait wasmtime_wasi_http::p3::WasiHttpCtx\">WasiHttpCtx</a> for <a class=\"struct\" href=\"spin_factor_outbound_http/struct.InstanceState.html\" title=\"struct spin_factor_outbound_http::InstanceState\">InstanceState</a>",0]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[386]}
