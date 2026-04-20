629eaa8d91793d83e786e36e8fb8b7ef1b827c2b
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2026-04-20 18:38:03 +0000

    Update docs/spin/main

--- docs/spin/main/trait.impl/spin_world/spin/redis/redis/trait.HostConnectionWithStore.js
+++ docs/spin/main/trait.impl/spin_world/spin/redis/redis/trait.HostConnectionWithStore.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["spin_factor_outbound_redis",[["impl HostConnectionWithStore for <a class=\"struct\" href=\"spin_factor_outbound_redis/struct.RedisFactorData.html\" title=\"struct spin_factor_outbound_redis::RedisFactorData\">RedisFactorData</a>",0]]]]);
+    const implementors = Object.fromEntries([["spin_factor_outbound_redis",[["impl <a class=\"trait\" href=\"spin_world/spin/redis/redis/trait.HostConnectionWithStore.html\" title=\"trait spin_world::spin::redis::redis::HostConnectionWithStore\">HostConnectionWithStore</a> for <a class=\"struct\" href=\"spin_factor_outbound_redis/struct.RedisFactorData.html\" title=\"struct spin_factor_outbound_redis::RedisFactorData\">RedisFactorData</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[236]}
+//{"start":59,"fragment_lengths":[403]}
