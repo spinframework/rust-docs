b9b7c81a8df83fe30e7159dc3ee44d19ff5cdd75
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2026-04-20 02:40:19 +0000

    Update docs/spin/main

--- docs/spin/main/trait.impl/spin_factors/runtime_factors/trait.HasInstanceBuilder.js
+++ docs/spin/main/trait.impl/spin_factors/runtime_factors/trait.HasInstanceBuilder.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["spin_runtime_factors",[["impl <a class=\"trait\" href=\"spin_factors/runtime_factors/trait.HasInstanceBuilder.html\" title=\"trait spin_factors::runtime_factors::HasInstanceBuilder\">HasInstanceBuilder</a> for <a class=\"struct\" href=\"spin_runtime_factors/struct.TriggerFactorsInstanceBuilders.html\" title=\"struct spin_runtime_factors::TriggerFactorsInstanceBuilders\">TriggerFactorsInstanceBuilders</a>",0]]]]);
+    const implementors = Object.fromEntries([["spin_runtime_factors",[["impl HasInstanceBuilder for <a class=\"struct\" href=\"spin_runtime_factors/struct.TriggerFactorsInstanceBuilders.html\" title=\"struct spin_runtime_factors::TriggerFactorsInstanceBuilders\">TriggerFactorsInstanceBuilders</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[415]}
+//{"start":59,"fragment_lengths":[258]}
--- docs/spin/main/trait.impl/spin_world/wasi/config/store/trait.Host.js
+++ docs/spin/main/trait.impl/spin_world/wasi/config/store/trait.Host.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["spin_factor_variables",[["impl <a class=\"trait\" href=\"spin_world/wasi/config/store/trait.Host.html\" title=\"trait spin_world::wasi::config::store::Host\">Host</a> for <a class=\"struct\" href=\"spin_factor_variables/struct.InstanceState.html\" title=\"struct spin_factor_variables::InstanceState\">InstanceState</a>",0]]],["spin_world",[]]]);
+    const implementors = Object.fromEntries([["spin_factor_variables",[["impl Host for <a class=\"struct\" href=\"spin_factor_variables/struct.InstanceState.html\" title=\"struct spin_factor_variables::InstanceState\">InstanceState</a>",0]]],["spin_world",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[327,18]}
+//{"start":59,"fragment_lengths":[196,18]}
