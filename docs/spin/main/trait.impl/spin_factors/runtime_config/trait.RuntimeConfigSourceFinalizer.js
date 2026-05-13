67545f833331d99a3b710399e082fee7d88fa537
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2026-05-13 23:46:18 +0000

    Update docs/spin/main

--- docs/spin/main/trait.impl/spin_factors/runtime_config/trait.RuntimeConfigSourceFinalizer.js
+++ docs/spin/main/trait.impl/spin_factors/runtime_config/trait.RuntimeConfigSourceFinalizer.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["spin_factors",[]],["spin_runtime_config",[["impl RuntimeConfigSourceFinalizer for <a class=\"struct\" href=\"spin_runtime_config/struct.TomlRuntimeConfigSource.html\" title=\"struct spin_runtime_config::TomlRuntimeConfigSource\">TomlRuntimeConfigSource</a>&lt;'_, '_&gt;",0]]]]);
+    const implementors = Object.fromEntries([["spin_factors",[]],["spin_runtime_config",[["impl <a class=\"trait\" href=\"spin_factors/runtime_config/trait.RuntimeConfigSourceFinalizer.html\" title=\"trait spin_factors::runtime_config::RuntimeConfigSourceFinalizer\">RuntimeConfigSourceFinalizer</a> for <a class=\"struct\" href=\"spin_runtime_config/struct.TomlRuntimeConfigSource.html\" title=\"struct spin_runtime_config::TomlRuntimeConfigSource\">TomlRuntimeConfigSource</a>&lt;'_, '_&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[19,259]}
+//{"start":59,"fragment_lengths":[19,434]}
