fe975a44c3a7e88dbca01013d30ef7e5cabbe9e8
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2026-05-27 19:56:20 +0000

    Update docs/spin/main

--- docs/spin/main/trait.impl/spin_world/fermyon/spin2_0_0/mysql/trait.HostConnection.js
+++ docs/spin/main/trait.impl/spin_world/fermyon/spin2_0_0/mysql/trait.HostConnection.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["spin_factor_outbound_mysql",[["impl&lt;C: <a class=\"trait\" href=\"spin_factor_outbound_mysql/client/trait.Client.html\" title=\"trait spin_factor_outbound_mysql::client::Client\">Client</a>&gt; <a class=\"trait\" href=\"spin_world/fermyon/spin2_0_0/mysql/trait.HostConnection.html\" title=\"trait spin_world::fermyon::spin2_0_0::mysql::HostConnection\">HostConnection</a> for <a class=\"struct\" href=\"spin_factor_outbound_mysql/struct.InstanceState.html\" title=\"struct spin_factor_outbound_mysql::InstanceState\">InstanceState</a>&lt;C&gt;",0]]],["spin_world",[]]]);
+    const implementors = Object.fromEntries([["spin_factor_outbound_mysql",[["impl&lt;C: <a class=\"trait\" href=\"spin_factor_outbound_mysql/client/trait.Client.html\" title=\"trait spin_factor_outbound_mysql::client::Client\">Client</a>&gt; HostConnection for <a class=\"struct\" href=\"spin_factor_outbound_mysql/struct.InstanceState.html\" title=\"struct spin_factor_outbound_mysql::InstanceState\">InstanceState</a>&lt;C&gt;",0]]],["spin_world",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[553,18]}
+//{"start":59,"fragment_lengths":[390,18]}
