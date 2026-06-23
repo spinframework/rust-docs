4805e26a16e4a12c554fc71b79a5bc25a15d9ff2
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2026-06-23 22:33:40 +0000

    Update docs/spin/main

--- docs/spin/main/trait.impl/spin_world/spin/postgres4_2_0/postgres/trait.HostConnectionWithStore.js
+++ docs/spin/main/trait.impl/spin_world/spin/postgres4_2_0/postgres/trait.HostConnectionWithStore.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["spin_factor_outbound_pg",[["impl&lt;CF: <a class=\"trait\" href=\"spin_factor_outbound_pg/client/trait.ClientFactory.html\" title=\"trait spin_factor_outbound_pg::client::ClientFactory\">ClientFactory</a>&gt; HostConnectionWithStore for <a class=\"struct\" href=\"spin_factor_outbound_pg/struct.PgFactorData.html\" title=\"struct spin_factor_outbound_pg::PgFactorData\">PgFactorData</a>&lt;CF&gt;",0]]]]);
+    const implementors = Object.fromEntries([["spin_factor_outbound_pg",[["impl&lt;CF: <a class=\"trait\" href=\"spin_factor_outbound_pg/client/trait.ClientFactory.html\" title=\"trait spin_factor_outbound_pg::client::ClientFactory\">ClientFactory</a>&gt; <a class=\"trait\" href=\"spin_world/spin/postgres4_2_0/postgres/trait.HostConnectionWithStore.html\" title=\"trait spin_world::spin::postgres4_2_0::postgres::HostConnectionWithStore\">HostConnectionWithStore</a> for <a class=\"struct\" href=\"spin_factor_outbound_pg/struct.PgFactorData.html\" title=\"struct spin_factor_outbound_pg::PgFactorData\">PgFactorData</a>&lt;CF&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[404]}
+//{"start":59,"fragment_lengths":[593]}
