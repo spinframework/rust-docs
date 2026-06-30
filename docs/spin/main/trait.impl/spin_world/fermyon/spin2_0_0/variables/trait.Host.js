9390a1a9afe105a19bb2c68798373fe0a945c0b5
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2026-06-30 03:22:30 +0000

    Update docs/spin/main

--- docs/spin/main/trait.impl/spin_world/fermyon/spin2_0_0/variables/trait.Host.js
+++ docs/spin/main/trait.impl/spin_world/fermyon/spin2_0_0/variables/trait.Host.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["spin_factor_variables",[["impl <a class=\"trait\" href=\"spin_world/fermyon/spin2_0_0/variables/trait.Host.html\" title=\"trait spin_world::fermyon::spin2_0_0::variables::Host\">Host</a> for <a class=\"struct\" href=\"spin_factor_variables/struct.InstanceState.html\" title=\"struct spin_factor_variables::InstanceState\">InstanceState</a>",0]]],["spin_world",[]]]);
+    const implementors = Object.fromEntries([["spin_factor_variables",[["impl Host for <a class=\"struct\" href=\"spin_factor_variables/struct.InstanceState.html\" title=\"struct spin_factor_variables::InstanceState\">InstanceState</a>",0]]],["spin_world",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[347,18]}
+//{"start":59,"fragment_lengths":[196,18]}
