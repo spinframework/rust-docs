f63baa946d5f70435ed2b4383bd1b24676945b4d
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2026-07-14 22:52:18 +0000

    Update docs/spin/main

--- docs/spin/main/trait.impl/spin_world/spin/redis/redis/trait.HostConnectionWithStore.js
+++ docs/spin/main/trait.impl/spin_world/spin/redis/redis/trait.HostConnectionWithStore.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["spin_factor_outbound_redis",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a>&gt; HostConnectionWithStore&lt;T&gt; for <a class=\"struct\" href=\"spin_factor_outbound_redis/struct.RedisFactorData.html\" title=\"struct spin_factor_outbound_redis::RedisFactorData\">RedisFactorData</a>",0]]]]);
+    const implementors = Object.fromEntries([["spin_factor_outbound_redis",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a>&gt; <a class=\"trait\" href=\"spin_world/spin/redis/redis/trait.HostConnectionWithStore.html\" title=\"trait spin_world::spin::redis::redis::HostConnectionWithStore\">HostConnectionWithStore</a>&lt;T&gt; for <a class=\"struct\" href=\"spin_factor_outbound_redis/struct.RedisFactorData.html\" title=\"struct spin_factor_outbound_redis::RedisFactorData\">RedisFactorData</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[389]}
+//{"start":59,"fragment_lengths":[556]}
