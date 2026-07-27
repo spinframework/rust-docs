a0569f50a5ba7d8da0dd821bdf37380bc8fd8bb2
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2026-07-27 04:25:54 +0000

    Update docs/spin/main

---
+++ docs/spin/main/search.index/37acdbb583.js
@@ -0,0 +1,1 @@
+rn_("AUThrzoAAEhDAABRCQA7A0UcsGkEAHV2")
--- docs/spin/main/trait.impl/spin_core/store/trait.AsState.js
+++ docs/spin/main/trait.impl/spin_core/store/trait.AsState.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["spin_core",[]],["spin_factors_executor",[["impl&lt;T, U&gt; AsState for <a class=\"struct\" href=\"spin_factors_executor/struct.InstanceState.html\" title=\"struct spin_factors_executor::InstanceState\">InstanceState</a>&lt;T, U&gt;",0]]]]);
+    const implementors = Object.fromEntries([["spin_core",[]],["spin_factors_executor",[["impl&lt;T, U&gt; <a class=\"trait\" href=\"spin_core/store/trait.AsState.html\" title=\"trait spin_core::store::AsState\">AsState</a> for <a class=\"struct\" href=\"spin_factors_executor/struct.InstanceState.html\" title=\"struct spin_factors_executor::InstanceState\">InstanceState</a>&lt;T, U&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[16,224]}
+//{"start":59,"fragment_lengths":[16,333]}
